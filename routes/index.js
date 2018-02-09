c8679599c0de22380bf9289bffb8267d82307cb7
Hyunsoo Hong <hyh014@ucsd.edu>
2018-02-08 22:19:18 -0800

    Adding pages

---
+++ routes/index.js
@@ -0,0 +1,32 @@
+
+/*
+ * GET home page.
+ */
+
+exports.view = function(req, res){
+  res.render('index', {
+    "tabs": [
+      { "name": "Create New Entry",
+        "id": "entry"},
+      { "name": "Anxiety",
+        "id": "anxiety"},
+      { "name": "View Diary",
+        "id": "diary"},
+      {"name": "Show Statistics",
+        "id": "stat"},
+      {"name": "Settings",
+        "id": "setting"},
+      {"name": "Fun News",
+        "id": "news"},
+      { "name":"Community",
+        "id":"community"},
+      { "name":"Profile",
+        "id":"profile"
+      },
+      { "name":"Login",
+        "id": "login"},
+      {"name":"Help",
+        "id":"help"}
+    ]
+  });
+};
